40c18082a369713d47d6bdf6496a02e798f9f183
MATTHEWGHURBURRUN <1021566@student.sae.edu.au>
2021-07-12 04:52:12 +1000

    post audio
    added in aarons lines
    starting on character sprites in intro

--- Assets/Scripts/Testing.cs
+++ Assets/Scripts/Testing.cs
@@ -10,23 +10,36 @@ public class Testing : MonoBehaviour
     void Start()
     {
         dialogue = DialogueSystem.instance;
+	
     }
 
 public string[] s = new string[]
 	{		
-		"Sir! Our radar navigation system is offline!:Crew Member",
+			
+		"Sir! Our navigation system is offline!:Crew Member",
+			
 		"What! How the hell did that happen!?:The General",
+		
 		"We were having a water gun fight in the navigation room...:Crew Member",
-		"What the-!:The General",
-		"Go to the control room and put her in stationary!",
+		
+		"What the-! Go to the control room and put her in stationary!:The General",
+		
 		"Ohh... We-we had a water gun fight in that room too…:Crew Member",
-		"GOD DAMN IT! WHAT THE HELL ARE YOU ALL THINKING!?:The General",
-		"Turn on all exterior lights, and get Sergeant Oceana in here!",
-		"Ye-yes sir!:Crew Member",
+		
+		"WHAT IN TARNATION! WHAT THE HELL WERE YOU ALL THINKING!?:The General",
+		
+		"Turn on all exterior lights, and somebody get Sergeant Oceana in here!",
+		
+		"Ye-sir, yes sir!:Crew Member",
+		
 		"*excited dolphin noises*:Stg.Oceana’s",
-		"Okay Sergeant, we are moving into a naval minefield without a radar.So we need you to get out there and navigate, use your melon to echolocate,:The General",
+		
+		"Okay Sergeant, we are moving into a naval minefield without a radar.So we need you to get out there and navigate, so use your melon to echolocate,:The General",
+		
 		"NOW STEP UP AND SHOW ME YOUR WAR FACE!",
+		
 		"*dutiful dolphin noises*:Stg.Oceana’s"
+		
 		
 	};
 
@@ -46,8 +59,9 @@ public string[] s = new string[]
 				}
 				
 				Say(s[index]);
+				PlayLine();
 				index++;
-				
+				lineIndex++;
 			}
 		}
     }
@@ -60,7 +74,73 @@ public string[] s = new string[]
 		
 		dialogue.Say(speech, speaker);
 	}
-
+	
+	int lineIndex = 0;
+	
+	void PlayLine()
+	{
+		if(lineIndex == 0)
+		{
+			FindObjectOfType<AudioManager>().Play("01");
+		}
+		if(lineIndex == 1)
+		{
+			FindObjectOfType<AudioManager>().Stop("01");
+			FindObjectOfType<AudioManager>().Play("02");
+		}
+		if(lineIndex == 2)
+		{
+			FindObjectOfType<AudioManager>().Stop("02");
+			FindObjectOfType<AudioManager>().Play("03");
+		}
+		if(lineIndex == 3)
+		{
+			FindObjectOfType<AudioManager>().Stop("03");
+			FindObjectOfType<AudioManager>().Play("04");
+		}
+		if(lineIndex == 4)
+		{
+			FindObjectOfType<AudioManager>().Stop("04");
+			FindObjectOfType<AudioManager>().Play("05");
+		}
+		if(lineIndex == 5)
+		{
+			FindObjectOfType<AudioManager>().Stop("05");
+			FindObjectOfType<AudioManager>().Play("06");
+		}
+		if(lineIndex == 6)
+		{
+			FindObjectOfType<AudioManager>().Stop("06");
+			FindObjectOfType<AudioManager>().Play("07");
+		}
+		if(lineIndex == 7)
+		{
+			FindObjectOfType<AudioManager>().Stop("07");
+			FindObjectOfType<AudioManager>().Play("08");
+		}
+		if(lineIndex == 8)
+		{
+			FindObjectOfType<AudioManager>().Stop("08");
+			FindObjectOfType<AudioManager>().Play("09");
+		}
+		if(lineIndex == 9)
+		{
+			FindObjectOfType<AudioManager>().Stop("09");
+			FindObjectOfType<AudioManager>().Play("10");
+		}
+		if(lineIndex == 10)
+		{
+			FindObjectOfType<AudioManager>().Stop("10");
+			FindObjectOfType<AudioManager>().Play("11");
+		}
+		if(lineIndex == 11)
+		{
+			FindObjectOfType<AudioManager>().Stop("11");
+			FindObjectOfType<AudioManager>().Play("12");
+		}
+		
+		
+	}
 
 }
 
